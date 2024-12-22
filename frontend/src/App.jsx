f52b83ace5f999270d6f5e7e5dca868d0b4304a3
kolarenuka2024 <kolarenuka555@gmail.com>
2024-12-22 17:46:40 +0530

    home page is added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Signup from './components/signup';
+import Home from './components/home';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-      <div>
-        <h1>hello world</h1>
-      </div>
-      )
+    <Signup/>
+    //<Home/>
+  );
 }
-
 export default App
---
+++ frontend/src/components/home.jsx
@@ -0,0 +1,40 @@
+function Home() {    
+      return (
+          <div className="home-container">
+              <header>
+                  <div className="logo">My Website Logo</div>
+                  <div id="select">
+                      <select id="branch">
+                          <option value="">Branch</option>
+                          <optgroup > branch</optgroup>
+                          <option value="CME">CME</option>
+                          <option value="ECE">ECE</option>
+                          <option value="EEE">EEE</option>
+                          <option value="MECH">MECH</option>
+                      </select>
+                      <select id="subject" >
+                          <option value="">Subject</option>
+                          <option value="c">c</option>
+                          <option value="c++">c++</option>
+                          <option value="java">java</option>
+                          <option value="python">python</option>
+                          <option value="sql">sql</option>
+                      </select>
+                  </div>
+                  <div className="header-right">
+
+                  </div>
+              </header>
+              <div className="container">
+                  <h1>Welcome to Our Educational Platform</h1>
+                  <p>choose best for your future</p>
+                  <div class="button">
+                      <input type="text" name="quary" placeholder="search here"></input>
+                      <button type="submit">Search</button>
+                  </div>
+              </div>
+          </div> 
+      );
+    }
+    export default Home
+    
