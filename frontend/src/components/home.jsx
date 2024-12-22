f6ba9df01a9e35fbe7a5cfaa506195bb4d878ece
kolarenuka2024 <kolarenuka555@gmail.com>
2024-12-22 23:51:38 +0530

    added boxes in home page

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -31,6 +31,35 @@ function Home() {
           <input type="text" name="query" placeholder="Search here" />
           <button type="submit" className="button">Search</button>
       </div>
+      <div class="local">
+        <div class="box">
+            <h2>python</h2>
+            <button>check</button>
+       </div>
+        <div class="box">
+        <h2>java</h2>
+        <button>check</button>
+        </div>
+        <div class="box">
+        <h2>c++</h2>
+        <button>check</button>
+        </div>
+         </div><br></br><br></br><br></br><br></br>
+         <div class="local">
+         <div class="box">
+            <h2>c programming</h2>
+            <button>check</button>
+       </div>
+        <div class="box">
+        <h2>sql</h2>
+        <button>check</button>
+        </div>
+        <div class="box">
+        <h2>web designing</h2>
+        <button>check</button>
+        </div>
+        </div>
+
     </div>
   );
 }
