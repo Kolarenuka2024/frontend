25ed9c2c8148c1cb92970214ab27034b608075c9
laptop-nsth4ium bagyasri <laptop-nsth4ium.bagyasri@default.com>
2024-12-22 17:19:18 +0530

    signup page without popup

---
+++ frontend/src/components/signup.jsx
@@ -0,0 +1,17 @@
+import "../styles/signup.css"
+function Signup() {
+  return (
+      <div>
+        <header>
+          MyWebsite
+          <button>sign up</button>
+        </header>
+        <section className="container" >
+          <h1>Start reading you concepts are avaliable here</h1>
+          <button>Get Started</button>
+        </section>
+      </div>
+      )
+}
+
+export default Signup;
